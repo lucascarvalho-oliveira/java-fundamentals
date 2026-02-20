895b330a4406ff80ed0532dd728840a4db6715c3
Lucas Carvalho De Jesus Oliveira <lucascarvalhoptc06@gmail.com>
2026-02-20 14:38:31 -0300

    Finalizando a logica

--- VerificandoDistintos/VerificandoDistintos.java
+++ VerificandoDistintos/VerificandoDistintos.java
@@ -11,11 +11,18 @@ public class VerificandoDistintos {
 
         int[] numeros = new int[quantidade];
 
+        System.out.println();
         System.out.println("Informe os numeros");
         for (int i = 0; i < quantidade; i++) {
             numeros[i] = sc.nextInt();
         }
 
+        verificarDistinto(numeros, quantidade);
+
+        sc.close();
+    }
+
+    public static void verificarDistinto(int[] numeros, int quantidade) {
         boolean distinto = false;
         for (int i = 0; i < quantidade; i++) {
             for (int j = i + 1; j < quantidade; j++) {
@@ -28,10 +35,11 @@ public class VerificandoDistintos {
         }
 
         if (distinto) {
+            System.out.println();
             System.out.println("A lista é distinta");
         } else{
+            System.out.println();
             System.out.println("A lista não é distinta");
         }
-        sc.close();
     }
 }
